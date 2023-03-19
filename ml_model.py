df9f3ea9e1b5e5a2f3d8b3a0ea4893637bb7a75b
KKondpalle <kaustubhmech1994@gmail.com>
2023-03-19 18:19:25 +0530

    added 3 new ml code

---
+++ ml_model.py
@@ -0,0 +1,11 @@
+print("adding all ml related model here")
+
+def model1():
+    print("adding code for ML Model 1")
+
+def model2():
+    print("adding code for ML Model 2")
+
+def model3():
+    print("adding code for ML Model 3")
+
